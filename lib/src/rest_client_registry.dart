95e426fff71585d30b231d1d453afd19a8963427
Max Rozdobudko <max.rozdobudko@gmail.com>
2022-01-20 12:04:01 +0200

    fix: adds missing file

---
+++ lib/src/rest_client_registry.dart
@@ -0,0 +1,31 @@
+part of 'rest_client.dart';
+
+class RestClientRegistry {
+
+  static final List<RestClient> _registry = [];
+
+  static RestClient get() {
+    if (_registry.isNotEmpty) {
+      return _registry.removeAt(0);
+    }
+    return Rest.config.rest.builder();
+  }
+
+  static void clear() {
+    _registry.clear();
+  }
+
+  static void reuse(RestClient client) {
+    if (_registry.length > 2) {
+      return;
+    }
+    client._uri = null;
+    client._encoder = null;
+    client._decoder = null;
+    client._headers.clear();
+    client._authorization = null;
+    client._stub = null;
+    _registry.add(client);
+  }
+
+}
